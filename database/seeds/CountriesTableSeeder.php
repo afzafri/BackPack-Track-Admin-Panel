470fe43b6f7943aaf407a5d00b884c57dcbb5565
Afif Zafri <zafri_21596@yahoo.com>
2018-08-06 23:32:02 +0800

    countries table seeder, seed with data from the json file

---
+++ database/seeds/CountriesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Country;
+
+class CountriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $json = File::get("database/data/countries.json");
+        $data = json_decode($json);
+        foreach ($data as $obj)
+        {
+          Country::create(array(
+            'code' => $obj->code,
+            'name' => $obj->name
+          ));
+        }
+    }
+}
