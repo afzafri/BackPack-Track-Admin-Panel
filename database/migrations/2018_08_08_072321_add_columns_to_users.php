6311eb06fd8a63d59d0f5c5da89e8f91fbc91778
Afif Zafri <zafri_21596@yahoo.com>
2018-08-08 15:31:07 +0800

    added new columns to Users table

---
+++ database/migrations/2018_08_08_072321_add_columns_to_users.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('users', function($table) {
+          $table->string('username')->after('name');
+          $table->string('phone')->after('username');
+          $table->string('address')->after('phone');
+          $table->integer('country_id')->after('address');
+          $table->string('avatar_url')->after('password');
+          $table->string('role')->after('avatar_url');
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('users', function($table) {
+            $table->dropColumn('username');
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+            $table->dropColumn('country_id');
+            $table->dropColumn('avatar_url');
+            $table->dropColumn('role');
+        });
+    }
+}
