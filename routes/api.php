a68cbc1dee2c3f16dd205b2686d8776bad592f61
Afif Zafri <zafri_21596@yahoo.com>
2018-08-09 11:24:37 +0800

    move API routes from web to api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,38 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// ----------------- API ROUTES -----------------
+// List all countries names
+Route::get('/listCountries', 'APIController@listCountries');
+
+// Create new itinerary
+Route::post('/newItinerary', 'APIController@newItinerary');
+
+// List all itineraries
+Route::get('/listItineraries', 'APIController@listItineraries');
+
+// List itineraries for specific Country
+Route::get('/listItinerariesByCountry/{country_id}', 'APIController@listItinerariesByCountry');
+
+// List itineraries for specific User
+Route::get('/listItinerariesByUser/{user_id}', 'APIController@listItinerariesByUser');
+
+// Create activity
+// To-do: Upload photos
+Route::post('/newActivity', 'APIController@newActivity');
+
+// View activities for an itinerary
+Route::get('/viewActivities/{itinerary_id}', 'APIController@viewActivities');
+
+// Register new user
+
+// Login user
+
+// List dates and no of day for an itinerary
+
+// List comments for an itinerary
+
+// List comments for specific users
+
+// Calculate total budget for an Itinerary
--- routes/web.php
+++ routes/web.php
@@ -18,37 +18,3 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-// ----------------- API ROUTES -----------------
-// List all countries names
-Route::get('/api/listCountries', 'APIController@listCountries');
-
-// Create new itinerary
-Route::post('/api/newItinerary', 'APIController@newItinerary');
-
-// List all itineraries
-Route::get('/api/listItineraries', 'APIController@listItineraries');
-
-// List itineraries for specific Country
-Route::get('/api/listItinerariesByCountry/{country_id}', 'APIController@listItinerariesByCountry');
-
-// List itineraries for specific User
-Route::get('/api/listItinerariesByUser/{user_id}', 'APIController@listItinerariesByUser');
-
-// Create activity
-// To-do: Upload photos
-Route::post('/api/newActivity', 'APIController@newActivity');
-
-// View activities for an itinerary
-Route::get('/api/viewActivities/{itinerary_id}', 'APIController@viewActivities');
-
-// Register new user
-
-// Login user
-
-// List comments for an itinerary
-
-// List comments for specific users
-
-// Calculate total budget for an Itinerary
