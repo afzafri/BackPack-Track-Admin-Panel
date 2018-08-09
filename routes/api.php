bee57cc99fc0bf3c9214222cd8e42b772712442d
Afif Zafri <zafri_21596@yahoo.com>
2018-08-09 15:27:19 +0800

    List all comments for a specific users and add todo list

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,10 @@ Route::get('/listCountries', 'APIController@listCountries');
 // Create new itinerary
 Route::post('/newItinerary', 'APIController@newItinerary');
 
+// Edit and update itinerary
+
+// Delete itinerary
+
 // List all itineraries
 Route::get('/listItineraries', 'APIController@listItineraries');
 
@@ -36,6 +40,10 @@ Route::get('/listItinerariesByUser/{user_id}', 'APIController@listItinerariesByU
 // Create activity
 // To-do: Upload photos
 Route::post('/newActivity', 'APIController@newActivity');
+
+// Edit and Update activity
+
+// Delete activity
 
 // View activities for an itinerary
 Route::get('/viewActivities/{itinerary_id}', 'APIController@viewActivities');
@@ -54,6 +62,9 @@ Route::post('/newComment', 'APIController@newComment');
 Route::get('/listComments/{itinerary_id}', 'APIController@listComments');
 
 // List comments for specific users
+Route::get('/listCommentsByUser/{user_id}', 'APIController@listCommentsByUser');
 
 // Calculate total budget for an Itinerary
 Route::get('/getTotalBudget/{itinerary_id}', 'APIController@getTotalBudget');
+
+// Calculate total budget for each day of a trip
