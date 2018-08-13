b5a7ae7fcfbbce28ed5e4ce082a76ab349ef1806
Afif Zafri <zafri_21596@yahoo.com>
2018-08-13 18:31:52 +0800

    create a view to test the layout

---
+++ resources/views/testview.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.adminpanel')
+
+@section('title', 'Dashboard')
+
+@section('pageheader', 'Dashboard')
+
+@section('content')
+  <h1> Hi nama saya afif </h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/testview', function() {
+  return view('testview');
+});
