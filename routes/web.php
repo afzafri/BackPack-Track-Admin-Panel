1e745208caf6d962af2c636bfc255295e3a2c1d5
Afif Zafri <zafri_21596@yahoo.com>
2018-08-17 21:17:18 +0800

    added route for dashboard

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/dashboard', 'AdminController@index');
+
 Route::get('/testview', function() {
   return view('testview');
 });
