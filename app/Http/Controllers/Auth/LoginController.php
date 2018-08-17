3218050d4b2c03934f41cf12b8077aa044af3d12
Afif Zafri <zafri_21596@yahoo.com>
2018-08-17 21:08:13 +0800

    Redirect each user to the specific page after login based on role

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -48,5 +49,18 @@ class LoginController extends Controller
             $field => $request->get($email),
             'password' => $request->password,
         ];
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        if(Auth::user()->role == 'admin')
+        {
+          return redirect('testview');
+        }
+
+        if(Auth::user()->role == 'backpacker')
+        {
+          return redirect('home');
+        }
     }
 }
