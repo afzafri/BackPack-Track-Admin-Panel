7e72939ff6f270d166f8ac3e6816a4f6e9dbb7ee
Afif Zafri <zafri_21596@yahoo.com>
2018-08-19 16:07:12 +0800

    comments some functions

--- app/Http/Controllers/ItineraryController.php
+++ app/Http/Controllers/ItineraryController.php
@@ -15,6 +15,7 @@ class ItineraryController extends Controller
         $this->middleware(['auth','admin']);
     }
 
+    // List all itineraries
     public function index()
     {
         // get all the itineraries
@@ -38,67 +39,69 @@ class ItineraryController extends Controller
     // Get duration of the trip
     public function getDuration($itinerary_id)
     {
-      $dates = Activity::distinct()->where('itinerary_id', $itinerary_id)->get(['date']);
-      $nodays = count($dates);
-      $nonight = $nodays > 0 ? ($nodays-1) : 0;
-      $duration = $nodays."D".$nonight."N";
+        $dates = Activity::distinct()->where('itinerary_id', $itinerary_id)->get(['date']);
+        $nodays = count($dates);
+        $nonight = $nodays > 0 ? ($nodays-1) : 0;
+        $duration = $nodays."D".$nonight."N";
 
-      return $duration;
+        return $duration;
     }
 
     // Get total budgets of each trip
     public function getTotalBudget($itinerary_id)
     {
-      $totalbudget = Activity::where('itinerary_id', $itinerary_id)->sum('budget');
+        $totalbudget = Activity::where('itinerary_id', $itinerary_id)->sum('budget');
 
-      return $totalbudget;
+        return $totalbudget;
     }
 
+    // Delete an itinerary
     public function destroy(Request $request)
     {
-      $itinerary_id = $request->itinerary_id;
+        $itinerary_id = $request->itinerary_id;
 
-      // Delete the itinerary
-      $itinerary = Itinerary::find($itinerary_id);
-      $itinerary->delete();
+        // Delete the itinerary
+        $itinerary = Itinerary::find($itinerary_id);
+        $itinerary->delete();
 
-      // Delete the activities
-      // Delete all images
-      $pic_urls = json_decode(Activity::where('itinerary_id', $itinerary_id)->get(['pic_url']), true);
-      foreach ($pic_urls as $pic_url)
-      {
-        $pic_msg = $this->deletePhoto($pic_url['pic_url']);
-      }
+        // Delete the activities
+        // Delete all images
+        $pic_urls = json_decode(Activity::where('itinerary_id', $itinerary_id)->get(['pic_url']), true);
+        foreach ($pic_urls as $pic_url)
+        {
+          $pic_msg = $this->deletePhoto($pic_url['pic_url']);
+        }
 
-      // Delete the activities data
-      $activities = Activity::where('itinerary_id', $itinerary_id)->delete();
+        // Delete the activities data
+        $activities = Activity::where('itinerary_id', $itinerary_id)->delete();
 
 
-      return redirect('itineraries')->with('deletestatus', 'Delete itinerary ID: '.$itinerary_id.' success!');
+        return redirect('itineraries')->with('deletestatus', 'Delete itinerary ID: '.$itinerary_id.' success!');
     }
 
     // delete photo
     public function deletePhoto($url)
     {
-      $baseurl = asset('/').'storage/';
-      $result = Storage::disk('public')->delete(str_replace($baseurl,'',$url));
+        $baseurl = asset('/').'storage/';
+        $result = Storage::disk('public')->delete(str_replace($baseurl,'',$url));
 
-      if($result)
-        return "File deleted";
-      else
-        return "File delete failed.";
+        if($result)
+          return "File deleted";
+        else
+          return "File delete failed.";
     }
 
+    // View activitites of an itinerary
     public function view(Request $request)
     {
-      $itinerary_id = $request->itinerary_id;
+        $itinerary_id = $request->itinerary_id;
 
-      $itinerary = Itinerary::with(['user','country'])->find($itinerary_id);
-      $activities = Activity::where('itinerary_id', $itinerary_id)->get();
+        $itinerary = Itinerary::with(['user','country'])->find($itinerary_id);
+        $activities = Activity::where('itinerary_id', $itinerary_id)->get();
 
-      $result['itinerary'] = $itinerary;
-      $result['activities'] = $activities;
+        $result['itinerary'] = $itinerary;
+        $result['activities'] = $activities;
 
-      return view('activities', ['data' => $result]);
+        return view('activities', ['data' => $result]);
     }
 }
