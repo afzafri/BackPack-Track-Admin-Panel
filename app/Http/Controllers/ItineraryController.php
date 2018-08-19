4d8782a09fb223f0a3188c53e3d540dc11290b2c
Afif Zafri <zafri_21596@yahoo.com>
2018-08-19 13:07:39 +0800

    get and return all itineraries together with the durations

--- app/Http/Controllers/ItineraryController.php
+++ app/Http/Controllers/ItineraryController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Itinerary;
+use App\Activity;
+
 class ItineraryController extends Controller
 {
     public function __construct()
@@ -13,6 +16,32 @@ class ItineraryController extends Controller
 
     public function index()
     {
-        return view('itinerary');
+        // get all the itineraries
+        $itineraries = Itinerary::with(['country','user'])->get();
+
+        // get the durations for each itinerary
+        $durations = [];
+        foreach ($itineraries as $itinerary)
+        {
+          $duration = $this->getDuration($itinerary->id);
+          $durations[$itinerary->id] = $duration;
+        }
+
+        // insert the durations into the itineraries
+        //$itineraries->put('durations', $durations);
+
+        //return $itineraries;
+        return view('itineraries', ['itineraries' => $itineraries, 'durations' => collect($durations)]);
+    }
+
+    // Get duration of the trip
+    public function getDuration($itinerary_id)
+    {
+      $dates = Activity::distinct()->where('itinerary_id', $itinerary_id)->get(['date']);
+      $nodays = count($dates);
+      $nonight = $nodays > 0 ? ($nodays-1) : 0;
+      $duration = $nodays."D".$nonight."N";
+
+      return $duration;
     }
 }
