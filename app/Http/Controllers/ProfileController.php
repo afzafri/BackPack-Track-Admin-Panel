61b62ac57b7b3cf6729d0b477b50ebfad76bf82c
Afif Zafri <zafri_21596@yahoo.com>
2018-08-20 17:46:58 +0800

    Reuse API code for upload new avatar

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -52,54 +52,21 @@ class ProfileController extends Controller
     // Update profile picture
     public function updateAvatar(Request $request)
     {
-        $rules = array(
-          'avatar' => 'required|image',
-        );
+        $request->request->add(['user_id' => Auth::user()->id]);
 
-        $validator = Validator::make($request->all(), $rules);
+        $APIobj = new APIController();
+        $result = $APIobj->uploadAvatar($request);
 
-        if($validator->fails())
+        $result = json_decode($result, true);
+        if($result['code'] == 400)
         {
-            $errors = $validator->errors();
-            return redirect('profile')->with('errors', $errors);
+          $errors = new MessageBag($result['error']);
+
+          return redirect('profile')->with('errors', $errors);
         }
-        else
+        else if($result['code'] == 200)
         {
-            if($request->hasFile('avatar'))
-            {
-              // Get old avatar
-              $old_avatar = json_decode(User::where('id', Auth::user()->id)->get(['avatar_url']), true)[0]['avatar_url'];
-
-              // Upload new avatar
-              $pic_url = $request->file('avatar')->store('images/avatars', 'public');
-              $pic_url = asset('storage/'.$pic_url);
-
-              // Update new avatar url
-              $user = User::find(Auth::user()->id);
-              $user->avatar_url = $pic_url;
-              $user->save();
-
-              // Delete old avatar file
-              $del_avatar = $this->deletePhoto($old_avatar);
-
-              return redirect('profile')->with('success', "New profile picture uploaded!");
-            }
-            else
-            {
-              return redirect('profile')->with('errors', ['avatar' => 'No profile picture uploaded']);
-            }
+          return redirect('profile')->with('success', "New profile picture uploaded!");
         }
-    }
-
-    // delete photo
-    public function deletePhoto($url)
-    {
-      $baseurl = asset('/').'storage/';
-      $result = Storage::disk('public')->delete(str_replace($baseurl,'',$url));
-
-      if($result)
-        return "File deleted";
-      else
-        return "File delete failed.";
     }
 }
