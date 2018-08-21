025c6f9b15a2a332e67327fa23616ca566c21e3c
Afif Zafri <zafri_21596@yahoo.com>
2018-08-21 14:22:56 +0800

    Save article into database

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Validator;
 
 use App\Article;
 
@@ -25,5 +26,40 @@ class ArticleController extends Controller
     public function create()
     {
         return view('create_article');
+    }
+
+    // Insert article to database
+    public function store(Request $request)
+    {
+        $rules = array(
+          'title' => 'required|string|max:255',
+          'author' => 'required|string|max:255',
+          'date' => 'required|date',
+          'summary' => 'required|string|max:255',
+          'content' => 'required|string',
+        );
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if($validator->fails())
+        {
+            $errors = $validator->errors();
+
+            return redirect('articles/create')->with('errors', $errors);
+        }
+        else
+        {
+            $article = new Article;
+
+            $article->title = $request->title;
+            $article->author = $request->author;
+            $article->date = $request->date;
+            $article->summary = $request->summary;
+            $article->content = $request->content;
+
+            $article->save();
+
+            return redirect('articles/create')->with('success', "Article created!");
+        }
     }
 }
