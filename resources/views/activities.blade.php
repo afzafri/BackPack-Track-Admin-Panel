a9e0177fa6645bc657b7b2ec7c5b728cbd2e9e27
Afif Zafri <zafri_21596@yahoo.com>
2018-08-22 21:19:57 +0800

    Change the method to access the data in view since now returned object

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -15,16 +15,16 @@
   <li class="list-inline-item seprate">
       <span>/</span>
   </li>
-  <li class="list-inline-item active">Itinerary ID:{{ $data['itinerary']['id'] }}</li>
+  <li class="list-inline-item active">Itinerary ID:{{ $data->id }}</li>
 @endsection
 
-@section('pageheader', $data['itinerary']['title'])
+@section('pageheader', $data->title)
 
 @section('content')
 
   <h3 class="title-5 m-b-35">
-    By: {{ $data['itinerary']['user']['name'] }} <br>
-    Country: {{ $data['itinerary']['country']['name'] }}
+    By: {{ $data->user->name }} <br>
+    Country: {{ $data->country->name }}
   </h3>
 
   <div class="table-responsive table--no-card m-b-30">
@@ -40,35 +40,35 @@
               </tr>
           </thead>
           <tbody>
-              @foreach ($data['activities'] as $activities)
+              @foreach ($data->activities as $activities)
                   <tr>
                       <td>
-                          {{ date('d-m-Y', strtotime($activities['date'])) }} <br>
-                          {{ date('h:i a', strtotime($activities['time'])) }}
+                          {{ date('d-m-Y', strtotime($activities->date)) }} <br>
+                          {{ date('h:i a', strtotime($activities->time)) }}
                       </td>
-                      <td>{{ $activities['activity'] }}</td>
-                      <td>{{ $activities['description'] }}</td>
+                      <td>{{ $activities->activity }}</td>
+                      <td>{{ $activities->description }}</td>
                       <td>
-                          {{ $activities['place_name'] }} &nbsp;
-                          <a href="https://www.google.com/maps/?q={{ $activities['lat'] }},{{ $activities['lng'] }}" target="_blank" data-toggle="tooltip" data-placement="top" title="" data-original-title="View Location">
+                          {{ $activities->place_name }} &nbsp;
+                          <a href="https://www.google.com/maps/?q={{ $activities->lat }},{{ $activities->lng }}" target="_blank" data-toggle="tooltip" data-placement="top" title="" data-original-title="View Location">
                             <i class="fas fa-map-marker-alt"></i>
                           </a>
                       </td>
                       <td>
-                          @if ($activities['pic_url'] != null)
-                            <a href="{{ $activities['pic_url'] }}" data-lightbox="activities-pic" data-title="{{ $activities['place_name'] }} - {{ $activities['activity'] }}">
-                              <img src="{{ $activities['pic_url'] }}">
+                          @if ($activities->pic_url != null)
+                            <a href="{{ $activities->pic_url }}" data-lightbox="activities-pic" data-title="{{ $activities->place_name }} - {{ $activities->activity }}">
+                              <img src="{{ $activities->pic_url }}">
                             </a>
                           @endif
                       </td>
-                      <td>{{ $activities['budget'] }}</td>
+                      <td>{{ $activities->budget }}</td>
                   </tr>
               @endforeach
                   <tr>
                       <th colspan="5" align="right">
                           <div style="float: right; text-align: right">Total Budget Spent</div>
                       </th>
-                      <td><b>{{ $totalbudget }}</b></td>
+                      <td><b>{{ $data->totalbudget }}</b></td>
                   </tr>
           </tbody>
       </table>
