df4f40a375a331c5c684ef65ee5538212b284b72
Afif Zafri <zafri_21596@yahoo.com>
2018-11-10 00:41:44 +0800

    fix wrong query

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -637,15 +637,13 @@ class APIController extends Controller
     public function listVisitedCountries()
     {
       $numdata = 6;
-      //$countries = Country::has('itinerary')->paginate($numdata);
 
       $countries = Itinerary::with(['country'])
                  ->select('country_id', DB::raw('count(*) as totalitineraries'))
                  ->groupBy('country_id')
-                 ->having('totalitineraries', '>', 0)
+                 ->havingRaw('count(*) > 0')
                  ->orderBy('totalitineraries', 'desc')
-                 ->take(5)
-                 ->get();
+                 ->paginate($numdata);
 
       return $countries;
     }
