90ef2e8d65403b1ab2b34dc297d9baa7143d798b
Afif Zafri <zafri_21596@yahoo.com>
2018-11-10 00:32:48 +0800

    return total itineraries for visited countries list

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -637,7 +637,12 @@ class APIController extends Controller
     public function listVisitedCountries()
     {
       $numdata = 6;
-      $countries = Country::has('itinerary')->paginate($numdata);
+      $countries = Itinerary::with(['country'])
+                 ->select('country_id', DB::raw('count(*) as totalitineraries'))
+                 ->groupBy('country_id')
+                 ->having('totalitineraries', '>', 0)
+                 ->orderBy('totalitineraries', 'desc')
+                 ->paginate($numdata);
       return $countries;
     }
 
