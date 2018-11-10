6ccd7ff8973d56cc33b43385edbd85e6fad8c981
Afif Zafri <zafri_21596@yahoo.com>
2018-11-10 13:15:54 +0800

    redesign widget

--- resources/views/currencyconverter.blade.php
+++ resources/views/currencyconverter.blade.php
@@ -5,9 +5,10 @@
       <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
    </head>
    <body>
+      <br>
       <!-- Currency Converter Script - FxExchangeRate.com  -->
-      <div style="width:248px;height:auto;border:1px solid #EA7E27;text-align:center;font-size:16px;font-family:sans-serif,Arial,Helvetica;border-top-right-radius:5px; border-top-left-radius:5px;background-color:#4BE8BA;">
-         <div style="width:100%; height:24px;padding:5px 0px 0px 0px;background-color:#3EC69E;font-weight:bold;"><a rel="nofollow" style="color:#FFFFFF;text-decoration:none;" href="https://www.fxexchangerate.com/">Currency Converter</a></div>
+      <div style="width:100%;height:auto;text-align:center;font-size:24px;font-family:sans-serif,Arial,Helvetica;">
+         
          <script type="text/javascript" src="https://w.fxexchangerate.com/converter.php?fm=USD&ft=EUR&lg=en&am=1&ty=1"></script>
       </div>
       <!--  End of Currency Converter Script -  FxExchangeRate.com -->
